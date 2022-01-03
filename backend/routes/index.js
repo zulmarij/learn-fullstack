00b3115dc24d0d705b929e19031e11f6d2043626
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2022-01-03 09:40:59 +0700

    backend

---
+++ backend/routes/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+} from "../controllers/Product.js";
+
+const router = express.Router();
+
+router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+router.post("/", createProduct);
+router.patch("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+export default router;
