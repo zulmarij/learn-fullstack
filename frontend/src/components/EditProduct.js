8a589da3cc445d9fa41e327f0ae0dd6bd7e7df0b
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2022-01-03 13:26:29 +0700

    frontend

---
+++ frontend/src/components/EditProduct.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+
+const EditProduct = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const updateProduct = async (e) => {
+    e.preventDefault();
+    await axios.patch(`http://localhost:5000/products/${id}`, {
+      title,
+      price,
+    });
+    navigate("/");
+  };
+
+  useEffect(() => {
+    getProductById();
+  }, []);
+
+  const getProductById = async () => {
+    const response = await axios.get(`http://localhost:5000/products/${id}`);
+    setTitle(response.data.title);
+    setPrice(response.data.price);
+  };
+
+  return (
+    <div>
+      <form onSubmit={updateProduct}>
+        <div className="field">
+          <label className="label">Title</label>
+          <input
+            className="input"
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="field">
+          <label className="label">Price</label>
+          <input
+            className="input"
+            type="text"
+            placeholder="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+        <div className="field">
+          <button className="button is-primary">Update</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default EditProduct;
